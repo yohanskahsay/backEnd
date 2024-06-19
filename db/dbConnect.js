a74cecbdbc0087d2f97235e944516fb15c4fab65
you@example.com <Your Name>
2024-06-19 12:09:25 -0700

    link update

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -6,7 +6,7 @@ async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
     .connect(
-        'mongodb+srv://aki:akidb1@cluster0.9ap6xao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+        'mongodb+srv://yohansk08:yohans08@cluster0.fklznrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
       {
         //   these are options to ensure that the connection is done properly
         useNewUrlParser: true,
